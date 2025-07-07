4ebded18ce6912199c8f0040ecc7862d43afa8de
Alexis Sotomayor <89486598+Alexiskeepcoding@users.noreply.github.com>
2025-07-06 21:02:38 -0500

    feat: implement payment confirmation flow with PayPhone integration and create PaymentService for backend communication. (fixing problems with pagoplux paybox)

--- src/app/donacion/payphone/page.tsx
+++ src/app/donacion/payphone/page.tsx
@@ -38,8 +38,6 @@ function PayphonePageContent() {
     router.back();
   };
 
-
-  console.log("Monto recibido:", monto);
   if (monto <= 0) {
     return (
       <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
